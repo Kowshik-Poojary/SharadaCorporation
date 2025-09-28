d04db9d70e29710770d77108867b7b55033aa4e8
Kowshik-Poojary <chandrapoojary1975@gmail.com>
2025-09-28 22:50:19 +0530

    Added white slider

--- Sharda Corporation/src/components/Navbar.jsx
+++ Sharda Corporation/src/components/Navbar.jsx
@@ -1,7 +1,12 @@
-// src/components/Navbar.jsx
-import React from "react";
+
+import React, { useState } from "react";
 
 const Navbar = () => {
+
+   const [activeLink, setActiveLink] = useState("Home"); // default active page
+
+  const links = ["Home", "About Us", "CSR", "Products","Catalogue","Gallery","Contact Us"];
+
   return (
     <nav className="bg-[#380f0e] text-white shadow-md h-20">
       <div className="container mx-auto flex justify-between items-center p-4">
@@ -9,14 +14,23 @@ const Navbar = () => {
         <h1 className="text-2xl font-bold">Sharda Corporation</h1>
 
         {/* Navigation Links */}
-        <ul className="flex space-x-6">
-          <li className="hover:text-yellow-400 cursor-pointer">Home</li>
-          <li className="hover:text-yellow-400 cursor-pointer">About Us</li>
-          <li className="hover:text-yellow-400 cursor-pointer">CSR</li>
-          <li className="hover:text-yellow-400 cursor-pointer">Products</li>
-          <li className="hover:text-yellow-400 cursor-pointer">Catalogue</li>
-          <li className="hover:text-yellow-400 cursor-pointer">Gallery</li>
-          <li className="hover:text-yellow-400 cursor-pointer">Contact Us</li>
+         <ul className="flex space-x-6">
+          {links.map((link) => (
+            <li
+              key={link}
+              className={`relative cursor-pointer pb-1 transition duration-300 
+                ${activeLink === link ? "text-yellow-400" : "hover:text-yellow-400"}`}
+              onClick={() => setActiveLink(link)}
+            >
+              {link}
+
+              {/* White slider underline */}
+              <span
+                className={`absolute left-0 bottom-0 h-[2px] bg-white transition-all duration-300
+                  ${activeLink === link ? "w-full" : "w-0 group-hover:w-full"}`}
+              ></span>
+            </li>
+          ))}
         </ul>
       </div>
     </nav>
