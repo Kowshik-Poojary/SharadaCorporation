752afc4651200ea3a215003b4dff03cbed43ad81
Kowshik-Poojary <chandrapoojary1975@gmail.com>
2025-09-29 10:52:19 +0530

    Footer added:

--- Sharda Corporation/src/App.jsx
+++ Sharda Corporation/src/App.jsx
@@ -11,14 +11,17 @@ import Products from "./components/Products";
 import Catalogues from "./components/Catalogues";
 import Gallerys from "./components/Gallerys";
 import Contact from "./components/Contact";
+import Footer from "./components/footer";
 
 function App() {
   return (
     <Router>
+      <div className="flex flex-col min-h-screen">
       {/* Navbar */}
       <Navbar />
 
       {/* Routes */}
+      <div className="flex-grow">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -28,6 +31,9 @@ function App() {
         <Route path="/gallery" element={<Gallerys />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+      </div>
+        <Footer />
+      </div>
     </Router>
   );
 }
