59f4497c341b7fabef34faba2e72dd757c05cc41
Kowshik-Poojary <chandrapoojary1975@gmail.com>
2025-10-18 23:31:39 +0530

    Removed unwanted text from Home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,8 +6,6 @@ import EnquiryMap from "./enquirymap";
 const Home = () => {
   return (
     <div className="p-10 text-center">
-      <h1 className="text-3xl font-bold">Welcome to Sharda Corporation</h1>
-      <p className="mt-4 text-gray-700">This is the Home page.</p>
       <CategorySlider/>
       <div className="mt-6">
         <EnquiryMap />
