0d580cdc9f1e06852bca14d0eb1263b31760b1bd
Kowshik-Poojary <chandrapoojary1975@gmail.com>
2025-10-18 23:32:40 +0530

    Removed unwanted text from contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,6 @@ const Contact = () => {
   return (
     <div className="p-10 text-center">
       <h1 className="text-3xl font-bold">Welcome to Sharda Corporation</h1>
-      <p className="mt-4 text-gray-700">This is the Contact Us page.</p>
       <EnquiryMap/>
     </div>
   );
