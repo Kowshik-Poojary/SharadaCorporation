d7280f0566c20d305dabb0b1e162c31b178a9fe4
Nikhil-2x <nikhilyadav101513@gmail.com>
2026-06-22 08:27:16 +0530

    added pagination to adminProducts

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient({
+export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: 1000 * 60 * 5,
